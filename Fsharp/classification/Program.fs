4581157e7e26baece304a7020ecf8b3d8599c99b
natsutan <YRW03704@nifty.com>
2020-07-23 21:40:27 +0900

    Sigmoid

---
+++ Fsharp/classification/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at https://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+let path = @"C:\home\myproj\StatisticalMLwith100\Fsharp\classification\fig\"
+
+[<EntryPoint>]
+let main argv =
+    
+    sigmoid.plotSigmoid path|> ignore
+
+    0 // return an integer exit code
---
+++ Fsharp/classification/sigmoid.fs
@@ -0,0 +1,22 @@
+﻿module sigmoid
+open FSharp.Charting
+
+let sigmoid beta x :float=
+    exp(x * beta) / (1.0 + exp(beta * x))
+
+    
+
+let plotSigmoid path =
+    let betas = [ 0.0; 0.2; 0.5; 1.2; 2.0; 10.0 ]
+    let mutable chart_list = []
+
+    for beta in betas do
+        let sigm = sigmoid beta
+        let chart = Chart.Line [for x in -10.0 .. 0.1 .. 10.0 -> x, sigm x ]
+        chart_list <- chart :: chart_list
+
+    let chart_comb = Chart.Combine chart_list
+    let output_file = path + @"sigmoid.png"
+    Chart.Save output_file chart_comb
+
+    None
