3c1e1beb462ba90948e6b2858bddf1ea727a8365
natsutan <YRW03704@nifty.com>
2020-08-07 21:57:59 +0900

    work

--- Fsharp/classification/Program.fs
+++ Fsharp/classification/Program.fs
@@ -1,11 +1,15 @@
 ﻿// Learn more about F# at https://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-let path = @"C:\home\myproj\StatisticalMLwith100\Fsharp\classification\fig\"
 
 [<EntryPoint>]
 let main argv =
+    let path = @"C:\home\myproj\StatisticalMLwith100\Fsharp\classification\fig\"
     
-    sigmoid.plotSigmoid path|> ignore
+    //sigmoid.plotSigmoid path|> ignore
+    let gd1 = discriminant_analysis.createGD(2.0, 2.0, 2.0, 2.0, 0.5)
+    let gd2 = discriminant_analysis.createGD(-3.0, -3.0, 1.0, 1.0, -0.8)
+    
+
 
     0 // return an integer exit code
