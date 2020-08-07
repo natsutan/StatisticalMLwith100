016c29fbed55efb672db481d5d3258962cc45b19
natsutan <YRW03704@nifty.com>
2020-08-07 23:00:47 +0900

    work

--- Fsharp/classification/Program.fs
+++ Fsharp/classification/Program.fs
@@ -1,15 +1,24 @@
 ﻿// Learn more about F# at https://fsharp.org
 // See the 'F# Tutorial' project for more help.
+open discriminant
 
+let path = @"C:\home\myproj\StatisticalMLwith100\Fsharp\classification\fig\"
+let N = 100
 
 [<EntryPoint>]
 let main argv =
-    let path = @"C:\home\myproj\StatisticalMLwith100\Fsharp\classification\fig\"
     
     //sigmoid.plotSigmoid path|> ignore
-    let gd1 = discriminant_analysis.createGD(2.0, 2.0, 2.0, 2.0, 0.5)
-    let gd2 = discriminant_analysis.createGD(-3.0, -3.0, 1.0, 1.0, -0.8)
-    
+    let gd1 = {GaussDistri.cx = 2.0; GaussDistri.cy = 2.0; 
+               GaussDistri.sigma_1 = 2.0; GaussDistri.sigma_2 = 2.0; 
+               GaussDistri.rho = 0.5}
+    let gd2 = {GaussDistri.cx = -3.0; GaussDistri.cy = -3.0; 
+                GaussDistri.sigma_1 = 1.0; GaussDistri.sigma_2 = 1.0; 
+                GaussDistri.rho = -0.8}
+ 
+    let data1 = discriminant.generateData(gd1, N)
+    let data2 = discriminant.generateData(gd2, N)
 
+    discriminant.plot(data1, data2, path) |> ignore
 
     0 // return an integer exit code
---
+++ Fsharp/classification/discriminant.fs
@@ -0,0 +1,37 @@
+﻿module discriminant
+open MathNet.Numerics.Distributions
+open FSharp.Charting
+
+type GaussDistri = 
+    { cx :float
+      cy :float
+      sigma_1 :float
+      sigma_2 :float
+      rho: float
+        }
+
+//Pythonのrandn
+let randn():double = 
+    Normal(mean=0.0, stddev=1.0).Sample()
+
+
+let createGD(x:float, y:float,  s1:float,  s2:float,  rho:float ) : GaussDistri =
+    {cx = x; cy = y; sigma_1 = s1; sigma_2 = s2; rho = rho}
+
+
+let generateData(gd:GaussDistri, N:int) = 
+    [| for _ in 1 .. N -> (gd.sigma_1 * randn() + gd.cx, gd.sigma_2 * randn() + gd.cy ) |]
+    
+
+let plot(data1:System.Tuple<double, double>[], data2:System.Tuple<double, double>[], path) = 
+    let chart1 = Chart.Point data1
+    let chart2 = Chart.Point data2
+
+    let chart = Chart.Combine [chart1; chart2]
+    let output_file = path + @"discri.png"    
+    Chart.Save output_file chart
+    
+    printf "%s\n"  output_file
+
+    None
+    
